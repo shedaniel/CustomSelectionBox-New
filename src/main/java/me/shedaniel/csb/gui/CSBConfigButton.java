f7d91ccd0766b00a9429d6b68abba4d3e1299702
geni <me@geni.site>
2019-04-12 20:25:30 +0300

    replace z in CSBConfigButton with width (how did i even think it was Z?!)

--- src/main/java/me/shedaniel/csb/gui/CSBConfigButton.java
+++ src/main/java/me/shedaniel/csb/gui/CSBConfigButton.java
@@ -4,7 +4,7 @@ import me.shedaniel.csb.CSB;
 import net.minecraft.client.gui.widget.ButtonWidget;
 
 public class CSBConfigButton extends ButtonWidget {
-    public CSBConfigButton(int x, int y, int z, int height, String message) {
-        super(x, y, z, height, message, widget -> CSB.openSettingsGUI());
+    public CSBConfigButton(int x, int y, int width, int height, String message) {
+        super(x, y, width, height, message, widget -> CSB.openSettingsGUI());
     }
 }
